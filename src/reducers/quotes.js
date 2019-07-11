2561c55f7afe7e4c65358acbdcec9b2645df64db
Lizzie <lchan217@gmail.com>
2019-07-11 08:27:17 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,35 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type){
+    case 'ADD_QUOTE':
+      const newState = [...state, action.quote]
+      return newState
+    case 'REMOVE_QUOTE':
+      const tempState = [...state]
+      return tempState.filter(quote => quote.id !== action.quoteId)
+    // case 'UPVOTE_QUOTE':
+    //   const addUpvoteState = [...state]
+    //   const quote = addUpvoteState.find(quote => quote.id === action.quoteId)
+    //   quote.votes++
+    //   return [...addUpvoteState]
+    case 'UPVOTE_QUOTE':
+      const addUpvoteState = [...state]
+      const quotes = addUpvoteState.map(quote => {
+        if (quote.id === action.quoteId){
+          quote.votes++
+        }
+        return quote
+      })
+      return quotes
+    case 'DOWNVOTE_QUOTE':
+      const addDownvoteState = [...state]
+      const quotesdown = addDownvoteState.map(quote => {
+        if (quote.id === action.quoteId && quote.votes > 0 ){
+          quote.votes--
+        }
+        return quote
+      })
+      return quotesdown
+    default:
+      return state;
+  }
 }
