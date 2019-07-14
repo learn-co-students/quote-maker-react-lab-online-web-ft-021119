5901d20ba7934ba80529b52a43882ee9f83a5109
bbennett7 <bryn.bennett@gmail.com>
2019-07-14 00:27:01 -0700

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const QuoteCard = (props) =>
-  <div>
-    <div className="card card-inverse card-success card-primary mb-3 text-center">
-      <div className="card-block">
-        <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
-        </blockquote>
-      </div>
-      <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            className="btn btn-primary"
-          >
-            Upvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-          >
-            Downvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
+class QuoteCard extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  handleOnClickDown = event => {
+    let quoteId = event.target.dataset.id
+    this.props.downvoteQuote(quoteId)
+  }
+
+  handleOnClickUp = event => {
+    let quoteId = event.target.dataset.id
+    this.props.upvoteQuote(quoteId)
+  }
+
+  handleOnClick = event => {
+    let quoteId = event.target.dataset.id
+    this.props.removeQuote(quoteId)
+  }
+
+render() {
+  console.log(this.props)
+  return (
+    <div>
+      <div className="card card-inverse card-success card-primary mb-3 text-center">
+        <div className="card-block">
+          <blockquote className="card-blockquote">
+            {<p>{this.props.content}</p>}
+            {<footer>- author <cite title="Source Title">{this.props.content}</cite></footer>}
+          </blockquote>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div className="float-right">
+          <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+            <button
+              type="button"
+              className="btn btn-primary"
+              data-id={this.props.id}
+              onClick={this.handleOnClickUp}
+            >
+              Upvote
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-id={this.props.id}
+              onClick={this.handleOnClickDown}
+            >
+              Downvote
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger"
+              data-id={this.props.id}
+              onClick={this.handleOnClick}
+            >
+              <span data-id={this.props.id} aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          {<div>Votes: {this.props.votes}</div>}
       </div>
     </div>
-  </div>;
+  </div>
+  )}
+}
 
-export default QuoteCard;
+export default connect()(QuoteCard);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,27 @@
+import { addQuote } from '../actions/quotes';
+import { removeQuote } from '../actions/quotes';
+import { upvoteQuote } from '../actions/quotes';
+import { downvoteQuote } from '../actions/quotes';
+
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+    case "REMOVE_QUOTE":
+      let i = state.findIndex(quote => quote.id === action.quoteId);
+      state.splice(i, 1)
+      return [...state];
+    case "UPVOTE_QUOTE":
+      state.find(quote => quote.id === action.quoteId).votes += 1
+      return [...state];
+    case "DOWNVOTE_QUOTE":
+      if (state.find(quote => quote.id === action.quoteId).votes > 0) {
+        state.find(quote => quote.id === action.quoteId).votes -= 1
+      }
+      return [...state];
+    default:
+      return state;
+  }
 }
