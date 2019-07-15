a42d8658f3daef1fef876d158f0071d3963c8855
chrstphgrnr <christophgruener@me.com>
2019-07-15 17:09:17 -0600

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,30 @@
 export default (state = [], action) => {
-  return state;
+  let tempState;
+    switch (action.type) {
+      case 'ADD_QUOTE':
+        const newState = [...state, action.quote]
+        return newState;
+      case 'UPVOTE_QUOTE':
+        tempState = [...state]
+        return tempState.map(quote => {
+          if (quote.id === action.quoteId) {
+            quote.votes++
+          }
+          return quote;
+         })
+      case 'DOWNVOTE_QUOTE':
+        tempState = [...state];
+        return tempState.map(quote => {
+          if (quote.id === action.quoteId && quote.votes > 0) {
+            quote.votes--;
+          }
+          return quote;
+        })      
+      case 'REMOVE_QUOTE':
+        tempState = [...state]
+        return tempState.filter(quote => quote.id !== action.quoteId);
+      default: return state;
+    }
+
+    
 }
